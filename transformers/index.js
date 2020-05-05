706740d8aa81c6f459ab1663e91037641855fac1
Arif Kurkchi <arif.kurkchi@rebilly.com>
2020-05-05 17:49:41 +0300

    Remove PaymentMethod option from PatchTransaction in favor of payment token

--- transformers/index.js
+++ transformers/index.js
@@ -1,5 +1,4 @@
-const xRebillyEnum = require('./xRebillyEnum');
 const xRebillyMerge = require('./xRebillyMerge');
 const xSortableEnum = require('./xSortableEnum');
 
-module.exports = [xRebillyEnum, xRebillyMerge, xSortableEnum];
+module.exports = [xRebillyMerge, xSortableEnum];
