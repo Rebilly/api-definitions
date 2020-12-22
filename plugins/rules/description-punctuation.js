0cc9ad9cca68eed6f78691c15e18feeaa5930588
Veaceslav Medvedev <slavcopost@gmail.com>
2020-12-22 15:00:43 +0100

    Improve rule

--- plugins/rules/description-punctuation.js
+++ plugins/rules/description-punctuation.js
@@ -5,10 +5,13 @@ function DescriptionPunctuation () {
   return {
     Schema(schema, { report, location }) {
       if (!schema.description) return;
-      if (!['.', '!'].includes(schema.description.slice(-1))) {
+      const lastChar = schema.description.trim().slice(-1);
+      if (!['.', '!'].includes(lastChar)) {
           report({
               message: 'The \`description\` should end with punctuation.',
               location: location.child(['description']),
+              content: schema.description,
+              lastChar,
           });
       }
     },
