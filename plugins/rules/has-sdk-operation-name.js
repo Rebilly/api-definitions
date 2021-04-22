c7c24b1847b54d3c38e80f6074f34d8ba75b7c24
Alberto Gualis <agualis@gmail.com>
2021-04-22 08:38:16 +0200

    Add custom lint rule to detect operations without SDK operation name

---
+++ plugins/rules/has-sdk-operation-name.js
@@ -0,0 +1,22 @@
+module.exports = HasSDKOperationName
+
+function requiredSdkOperationName() {
+    return (operation, {report, location, parent, type}) => {
+        //Skip webhook operations validation
+        if (location.source.absoluteRef.includes('/openapi/webhooks/')) return;
+
+        //Skip experimental operations validation
+        if (parent.servers && parent.servers[0].url === 'https://api.rebilly.com/experimental') return;
+        
+        if (!operation['x-sdk-operation-name']) {
+            report({message: `${operation.operationId} operation is missing required x-sdk-operation-name property`});
+        }
+    };
+}
+
+/** @type {import('@redocly/openapi-cli').OasRule} */
+function HasSDKOperationName() {
+    return {
+        Operation: requiredSdkOperationName(),
+    }
+}
