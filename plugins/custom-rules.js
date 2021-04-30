c5a1d90a07f39aeb4925f28906610577f59958dd
Demmie <2e3s19@gmail.com>
2021-04-30 16:11:58 -0400

    Check SDK operation name to be unique

--- plugins/custom-rules.js
+++ plugins/custom-rules.js
@@ -1,12 +1,12 @@
 const DescriptionPunctuation = require('./rules/description-punctuation');
-const HasSDKOperationName = require('./rules/has-sdk-operation-name');
+const HasUniqueSDKOperationName = require('./rules/has-unique-sdk-operation-name');
 const id = 'custom-rules';
 
 /** @type {import('@redocly/openapi-cli').CustomRulesConfig} */
 const rules = {
     oas3: {
         'description-punctuation': DescriptionPunctuation,
-        'has-sdk-operation-name': HasSDKOperationName,
+        'has-sdk-operation-name': HasUniqueSDKOperationName,
     },
 };
 
--- plugins/rules/has-unique-sdk-operation-name.js
+++ plugins/rules/has-unique-sdk-operation-name.js
@@ -1,21 +1,30 @@
-module.exports = HasSDKOperationName
+module.exports = HasUniqueSdkOperationName
 
 function requiredSdkOperationName() {
+    const seenOperationNames = new Set();
+
     return (operation, {report, location, parent, type}) => {
         //Skip webhook operations validation
         if (location.source.absoluteRef.includes('/openapi/webhooks/')) return;
 
         //Skip experimental operations validation
         if (parent.servers && parent.servers[0].url === 'https://api.rebilly.com/experimental') return;
-        
+
         if (!operation['x-sdk-operation-name']) {
             report({message: `${operation.operationId} operation is missing required x-sdk-operation-name property`});
         }
+        if (seenOperationNames.has(operation['x-sdk-operation-name'])) {
+            report({
+                message: 'Every operation must have a unique `operationId`.',
+                location: location.child([operation.operationId]),
+            });
+        }
+        seenOperationNames.add(operation['x-sdk-operation-name']);
     };
 }
 
 /** @type {import('@redocly/openapi-cli').OasRule} */
-function HasSDKOperationName() {
+function HasUniqueSdkOperationName() {
     return {
         Operation: requiredSdkOperationName(),
     }
