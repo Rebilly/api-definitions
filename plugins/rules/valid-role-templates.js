f93c4c9f525ed71848ac9f247d79364e1c970565
Alberto Gualis <agualis@gmail.com>
2021-04-30 11:48:14 +0200

    Add all tempalte roles to combined definition

--- plugins/rules/valid-role-templates.js
+++ plugins/rules/valid-role-templates.js
@@ -1,17 +1,37 @@
 module.exports = ValidRoleTemplates
 
 const seenOperations = new Set();
+let seenPermissions = new Set();
 
 function registerOperations() {
-    return (operation) => {
+    return (operation, {report}) => {
+        const validateOperationAppearsInPermissions = (operationId) => {
+            if (!seenPermissions.has(operationId)) {
+                report({message: `OperationId ${operationId} not found in x-roleTemplates`})
+            }
+        }
+        
         seenOperations.add(operation.operationId);
+        // validateOperationAppearsInPermissions(operation.operationId);
+    };
+}
+
+function registerPermissions() {
+    return (root) => {
+        const roleTemplates = root['x-roleTemplates'];
+        if (!roleTemplates) return
+
+        roleTemplates.forEach(role => {
+            const rolePermissions = new Set(role.permissions);
+            seenPermissions = new Set([...seenPermissions, ...rolePermissions]);
+        });
     };
 }
 
 function hasValidRoleTemplates() {
     return (root, {report}) => {
         const roleTemplates = root['x-roleTemplates'];
-        if (!root['x-roleTemplates']) return
+        if (!roleTemplates) return
         roleTemplates.forEach(role => {
             const roleId = role.id;
             const permissions = role.permissions;
@@ -34,6 +54,7 @@ function ValidRoleTemplates() {
     return {
         Operation:  registerOperations(),
         DefinitionRoot: {
+            enter: registerPermissions(),
             leave: hasValidRoleTemplates(),
         },
     }
