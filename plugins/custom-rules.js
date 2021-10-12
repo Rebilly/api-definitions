c9725bec289d86e444942b6f8b353a7a6143bd80
VladimirBerdnik <vladimir.berdnik@rebilly.com>
2021-10-12 18:30:26 +0600

    Define rule

--- plugins/custom-rules.js
+++ plugins/custom-rules.js
@@ -1,5 +1,6 @@
 const DescriptionPunctuation = require('./rules/description-punctuation');
 const HasSDKOperationName = require('./rules/has-sdk-operation-name');
+const NoUnusedTags = require('./rules/no-unused-tags');
 const id = 'custom-rules';
 
 /** @type {import('@redocly/openapi-cli').CustomRulesConfig} */
@@ -7,6 +8,7 @@ const rules = {
     oas3: {
         'description-punctuation': DescriptionPunctuation,
         'has-sdk-operation-name': HasSDKOperationName,
+        'no-unused-tags': NoUnusedTags,
     },
 };
 
