469bcc872b21eb623a0b74aed8dc9c0c8329768d
Adam Altman <adam@rebilly.com>
2022-07-26 22:32:40 -0500

    chore: remove custom rule covered by assertions

--- plugins/custom-rules.js
+++ plugins/custom-rules.js
@@ -1,5 +1,4 @@
 const DescriptionPunctuation = require('./rules/description-punctuation');
-const HasSDKOperationName = require('./rules/has-sdk-operation-name');
 const NoUnusedTags = require('./rules/no-unused-tags');
 const id = 'custom-rules';
 
@@ -7,7 +6,6 @@ const id = 'custom-rules';
 const rules = {
     oas3: {
         'description-punctuation': DescriptionPunctuation,
-        'has-sdk-operation-name': HasSDKOperationName,
         'no-unused-tags': NoUnusedTags,
     },
 };
