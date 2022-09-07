6e10b0a522e952dc57c13546e80e87c160b2b886
Arif Kurkchi <justnero.ru@yandex.ru>
2022-09-07 12:58:38 +0300

    Add subscription related examples

---
+++ openapi/code_samples/PHP-SDK-3.0/subscriptions/post.php
@@ -0,0 +1,19 @@
+$service = new Rebilly\Sdk\CoreService($client);
+
+$order = SubscriptionOrder::from()
+    ->setWebsiteId('websiteId')
+    ->setCustomerId('customerId')
+    ->setItems([
+        OrderItem::from()
+            ->setPlan(
+                OriginalPlan::from()
+                    ->setId('planId')
+            )
+            ->setQuantity(1),
+    ]);
+
+try {
+    $order = $service->subscriptions()->create($order);
+} catch (Rebilly\Sdk\Exception\DataValidationException $e) {
+    print_r($e->getValidationErrors());
+}
