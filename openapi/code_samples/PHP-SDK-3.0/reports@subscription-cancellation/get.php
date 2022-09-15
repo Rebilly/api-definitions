695a6be0d47e6b647a7d4c7d0e80cc3360fa8c73
N. Fasano <46872160+n-fasano@users.noreply.github.com>
2022-09-15 09:49:25 +0300

    Add code samples for Reports API (#1025)
    Co-authored-by: Alexander Shkarpetin <ashkarpetin@gmail.com>
    Co-authored-by: Arif Kurkchi <11618962+justnero@users.noreply.github.com>

---
+++ openapi/code_samples/PHP-SDK-3.0/reports@subscription-cancellation/get.php
@@ -0,0 +1,8 @@
+$service = new Rebilly\Sdk\ReportsService($client);
+
+$report = $service->reports()->getSubscriptionCancellation(
+    new DateTimeImmutable('2022-01-01'),
+    new DateTimeImmutable('now'),
+    'leadSource.source',
+    limit: 5,
+);
