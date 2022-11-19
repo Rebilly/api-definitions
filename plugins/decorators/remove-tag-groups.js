b2a43f13a2adf2e17d11317228196d03ca078e2b
Adam Altman <adam@rebilly.com>
2022-11-19 11:41:00 -0600

    feat: simplify to core and all APIs - leave original bundler for temporary backwards compatibility

---
+++ plugins/decorators/remove-tag-groups.js
@@ -0,0 +1,16 @@
+
+module.exports = RemoveTagGroups;
+
+/** @type {import('@redocly/cli').OasDecorator} */
+
+function RemoveTagGroups() {
+  return {
+    Root: {
+      leave(Root) {
+        if (Root['x-tagGroups']) {
+          delete Root['x-tagGroups'];
+        }
+      }
+    }
+  }
+};
