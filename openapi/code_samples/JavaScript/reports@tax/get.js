51fba64c1b4a57c1fa21608d752d59c2275c01c2
Simon Sorich <simon.sorich@rebilly.com>
2023-04-28 11:28:12 +0200

    Fix parameter naming

--- openapi/code_samples/JavaScript/reports@tax/get.js
+++ openapi/code_samples/JavaScript/reports@tax/get.js
@@ -1,6 +1,6 @@
 const params = {
-    bookedFrom: '2023-01-01 00:00-00',
-    bookedTo: '2023-03-31 23:59:59',
+    periodStart: '2023-01-01 00:00-00',
+    periodEnd: '2023-03-31 23:59:59',
     accountingMethod: 'cash',
     limit: 20,
     offset: 0,
