581ce5cac7b6eed507500b0c997d1febfe55164c
Alexander Shkarpetin <ashkarpetin@gmail.com>
2023-06-23 01:44:25 +0600

    Remove PostActivation

--- openapi/code_samples/JavaScript/activation@{token}/post.js
+++
@@ -1,7 +0,0 @@
-const token = '1234567890abcdefghijklmnop';
-
-const activation = await api.account.activate({token});
-
-// the request does not return any fields but
-// you can confirm the success using the status code
-console.log(activation.response.status); // 204
