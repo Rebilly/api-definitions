a8e0883d5b5170328f5ebeb5dd5eceeb71acecee
Lucas de Oliveira <mail@lucasxciv.dev>
2023-10-04 19:23:23 -0300

    Adjust PHP SDK v3 code samples

--- openapi/code_samples/PHP-SDK-3.0/customers/get.php
+++ openapi/code_samples/PHP-SDK-3.0/customers/get.php
@@ -1,5 +1,18 @@
 <?php
 
 $service = new \Rebilly\Sdk\CoreService($client);
-// all parameters are optional.
-$customers = $service->customers()->getAll(filter: 'firstName:John');
+
+$customersPaginator = $service->customers()->getAllPaginator(limit: 5);
+foreach ($customersPaginator as $customerPage) {
+    printf("Customers page %d/%d\n", $customersPaginator->key() + 1, $customersPaginator->count());
+    foreach ($customerPage as $customer) {
+        printf("Customer #%s: %s\n", $customer->getId(), $customer->getFirstName());
+    }
+}
+
+// OR
+
+$customers = $service->customers()->getAll(limit: 100);
+foreach ($customers as $customer) {
+    printf("Customer #%s: %s\n", $customer->getId(), $customer->getFirstName());
+}
--- openapi/code_samples/PHP-SDK-3.0/tags/get.php
+++ openapi/code_samples/PHP-SDK-3.0/tags/get.php
@@ -2,4 +2,17 @@
 
 $service = new \Rebilly\Sdk\CoreService($client);
 
-$tags = $service->tags()->getAll();
+$tagsPaginator = $service->tags()->getAllPaginator(limit: 5);
+foreach ($tagsPaginator as $tagPage) {
+    printf("Tags page %d/%d\n", $tagsPaginator->key() + 1, $tagsPaginator->count());
+    foreach ($tagPage as $tag) {
+        printf("Tag #%s: %s\n", $tag->getId(), $tag->getName());
+    }
+}
+
+// OR
+
+$tags = $service->tags()->getAll(limit: 100);
+foreach ($tags as $tag) {
+    printf("Tag #%s: %s\n", $tag->getId(), $tag->getName());
+}
