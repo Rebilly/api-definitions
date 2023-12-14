65472f7e7990b8b99ee0256835a931ebfbd03a4f
Alexander Shkarpetin <alexander.shkarpetin@rebilly.com>
2023-12-14 21:00:39 +0600

    Use FQCN in PHP SDK 3.x code samples (#1725)
    * Use FQCN in PHP SDK 3.x code samples
    
    * Suppress error
    
    * Fix psalm

--- openapi/code_samples/PHP/reports@cumulative-subscriptions/get.php
+++ openapi/code_samples/PHP/reports@cumulative-subscriptions/get.php
@@ -4,7 +4,7 @@ $service = new \Rebilly\Sdk\ReportsService($client);
 
 $report = $service->reports()->getCumulativeSubscriptions(
     'day',
-    new DateTimeImmutable('2022-01-01'),
-    new DateTimeImmutable('now'),
+    new \DateTimeImmutable('2022-01-01'),
+    new \DateTimeImmutable('now'),
     limit: 5,
 );
