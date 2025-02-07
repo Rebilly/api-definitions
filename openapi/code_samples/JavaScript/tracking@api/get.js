e1fb1210e64a6ef46882bff69c66db1b8ba75345
rebilly-machine-user <29335184+rebilly-machine-user@users.noreply.github.com>
2025-02-07 18:39:09 +0000

    Update API definitions

--- openapi/code_samples/JavaScript/tracking@api/get.js
+++ openapi/code_samples/JavaScript/tracking@api/get.js
@@ -16,8 +16,8 @@ secondCollection.items.forEach(entry => console.log(entry.fields.customerId));
 const firstFile = await api.tracking.downloadApiLogsCSV();
 
 // alternatively you can specify one or more of them
-const params = {limit: 20, offset: 100, sort: '-createdTime'};
-const secondFile = await api.tracking.downloadApiLogsCSV(params);
+const sortedParams = {limit: 20, offset: 100, sort: '-createdTime'};
+const secondFile = await api.tracking.downloadApiLogsCSV(sortedParams);
 
 // access the file data to view the CSV content
 console.log(secondFile.data);
