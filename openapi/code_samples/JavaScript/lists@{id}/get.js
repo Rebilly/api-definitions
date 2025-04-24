98842d860d8c248a8664f4d80af9a532782c2076
rebilly-machine-user <29335184+rebilly-machine-user@users.noreply.github.com>
2025-04-24 11:48:34 +0000

    Update API definitions

--- openapi/code_samples/JavaScript/lists@{id}/get.js
+++ openapi/code_samples/JavaScript/lists@{id}/get.js
@@ -1,5 +1,5 @@
 // get the latest version
-const lastest = await api.lists.get({id: 'foobar-001'});
+const latest = await api.lists.get({id: 'foobar-001'});
 
 // get an older version
 const older = await api.lists.get({id: 'foobar-001', version: 12});
